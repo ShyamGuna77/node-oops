038e47188523ac5d9b973f5beaa8d4aa3b72cbda
ShyamGuna77 <gshyamprasad2001@gmail.com>
2025-11-23 16:19:21 +0530

    added

---
+++ app.js
@@ -0,0 +1,10 @@
+let Car = new Object();
+
+Car.make = "Toyota";
+Car.model = "Corolla";
+Car.year = 2020;
+
+
+console.log(Car);console.log("Car Make: " + Car.make);
+console.log("Car Model: " + Car.model);
+console.log("Car Year: " + Car.year);
