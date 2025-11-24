bd317068054c3206c2fa8653389a88a832b71633
ShyamGuna77 <gshyamprasad2001@gmail.com>
2025-11-24 19:15:03 +0530

    dd

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-class Bank {
-  #balance = 1000;
+class Account {
+  #balance = 0;
+
+  constructor(name) {
+    this.name = name;
+  }
 
   deposit(amount) {
+    this.#validate(amount);
     this.#balance += amount;
+  }
+
+  withdraw(amount) {
+    this.#validate(amount);
+    if (amount > this.#balance) throw new Error("Not enough funds");
+    this.#balance -= amount;
+  }
+
+  #validate(amount) {
+    if (amount <= 0) throw new Error("Invalid amount");
   }
 
   getBalance() {
@@ -10,7 +25,12 @@ class Bank {
   }
 }
 
-const b = new Bank();
-b.deposit(200);
-b.getBalance();
-console.log(b.getBalance());
+class PremiumAccount extends Account {
+  deposit(amount) {
+    super.deposit(amount + 100); // bonus
+  }
+}
+
+const acc = new PremiumAccount("Sinister");
+acc.deposit(500);
+console.log(acc.getBalance()); // 600
